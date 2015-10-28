d9ef2547c82bbc601f1b2920a0c98fed68854a5d
“pkadams67” <paul.k.adams@me.com>
2015-10-28 16:41:14 -0600

    Network Controller Complete

---
+++ NetworkController.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkController.swift
+//  Weather
+//
+//  Created by Paul Kirk Adams on 10/28/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+
+    static let apiKey = "ca1c3fb65aa0e96d671d124200dbc579"
+    static let baseCityURL = "api.openweathermap.org/data/2.5/weather?q="
+//    static let baseZIPURL = "api.openweathermap.org/data/2.5/weather?zip="
+
+    static func searchURL(searchNameString: String) -> NSURL {
+        let escapedSearchNameString = searchNameString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet())
+        return NSURL(string: "\(baseCityURL)\(escapedSearchNameString)\(apiKey)")!
+    }
+
+    static func dataAtURL(dataAtURL: NSURL, completion: (resultData: NSData?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithURL(dataAtURL) { (resultData, _, error) -> Void in
+            if let error = error {
+                print("Data Task URL Retrieval Failure")
+                completion(resultData: nil)
+            } else {
+                completion(resultData: resultData)
+            }
+        }
+
+        dataTask.resume()
+    }
+
+}
